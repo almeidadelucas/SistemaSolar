9f2d6ecef10612b60aaae0f7d8c8a58e3c123000
LucasAlmeida <almeidadelucas@gmail.com>
2018-10-17 09:23:22 -0300

    Initial commit

---
+++ Assets/Scripts/GiraGira.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiraGira : MonoBehaviour {
+
+    public float x, y, z;
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 rotacao = new Vector3(x*Time.deltaTime, y*Time.deltaTime, z*Time.deltaTime); // x graus em x, y graus em y e z gruas em z
+                                                                                                // deltaTime = tempo desda última vez q foi chamado
+        transform.Rotate(rotacao);
+	}
+}
