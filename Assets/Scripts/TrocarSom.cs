864d84632ba6ca55f640cd3e85caf032004380bd
LucasAlmeida <almeidadelucas@gmail.com>
2018-10-18 10:34:15 -0300

    Adcição de Som nos Plaetas e dos movimentos de rotação e translação

---
+++ Assets/Scripts/TrocarSom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrocarSom : MonoBehaviour
+{
+
+    public AudioClip som;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        if (som != null)
+        {
+            GameObject s = GameObject.FindGameObjectWithTag("Som");
+            s.GetComponent<AudioSource>().clip = som;
+            s.GetComponent<AudioSource>().Play();
+        }
+    }
+}
